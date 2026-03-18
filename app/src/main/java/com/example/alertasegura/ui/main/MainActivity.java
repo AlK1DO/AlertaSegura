55c291d06a97e6a0764bcd7be74f87dd8997a928
AIK1DO <alk1do@users.noreply.github.com>
2026-03-18 18:57:26 -0500

    cambio de temas

--- app/src/main/java/com/example/alertasegura/ui/main/MainActivity.java
+++ app/src/main/java/com/example/alertasegura/ui/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
